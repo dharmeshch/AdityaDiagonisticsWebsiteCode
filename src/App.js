79787bd1da5239de48b8ec00b886b20f211351cd
skodumur <skodumur@asu.edu>
2019-01-31 16:31:40 -0700

    Added ham burger menu

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import {Navbar,Nav,NavItem,NavDropdown,MenuItem} from 'react-bootstrap';
 import './App.css';
 import About from './About/About.js'
 import ContentDetail from './Content/Content.js'
+import { slide as Menu } from 'react-burger-menu'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar collapseOnSelect navbar-default>
+      <Menu>
+        <a id="home" className="menu-item" href="/">Home</a>
+        <a id="about" className="menu-item" href="/about">About</a>
+        <a id="contact" className="menu-item" href="/contact">Contact</a>
+        <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+      </Menu>
+       <div id="page-wrap">
+       <Navbar collapseOnSelect navbar-default>
           <Navbar.Header>
             <Navbar.Brand>
               <a href="#brand">Aditya Diagnostics and Research Laboratory</a>
@@ -27,6 +35,7 @@ class App extends Component {
         </Navbar>
         <About />
         <ContentDetail />
+       </div>
       </div>
     );
   }
