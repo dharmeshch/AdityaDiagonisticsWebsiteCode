f8eb13033d08a724c494b7d20e1a2311e2eaebff
skodumur <skodumur@asu.edu>
2019-01-31 19:24:04 -0700

    Content Stylings

--- src/About/About.js
+++ src/About/About.js
@@ -5,19 +5,20 @@ import labPic from "../Images/LabPic1.jpg"
 import {Button} from 'react-bootstrap'
 const aboutContent = (props) =>{
     const style ={
-        backgroundColor:'green !important',
+        backgroundColor:'#60a5a5 !important',
         margin:'40px auto',
         width:'200px'
       }
     return(
         <div className="about">
-            <h1>Aditya Diagnostics and Research <br />Laboratory</h1>
-            <p>We provide a full range of medical diagnostics efficiently and with highest quality. A quality of service you can always Trust!
-                        Open today until 22:00</p>
-            <Button style={style} bsSize="large" block>
-                Get Directions
-            </Button>
-            <br />
+            <div className="introduction">
+                <span className="main-text">Aditya Diagnostics and Research <br />Laboratory</span>
+                <p className="normal-text">We provide a full range of medical diagnostics efficiently and with highest quality. A quality of service you can always Trust!
+                            Open today until 22:00</p>
+                <Button bsStyle="" className="btn-custom" bsSize="large">
+                    Get Directions
+                </Button>
+            </div>
             <img src={labPic} alt="LabPic" />
         </div>
     )
